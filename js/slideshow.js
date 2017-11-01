e505843de948689f68bf954638378d41b0c7d5b9
veersudhir83 <veersudhir83@gmail.com>
2017-11-01 17:31:23 +0530

    Updated images and alignments for slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,25 +3,28 @@ var slideIndex = 0;
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    //var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    if (slideIndex > slides.length) {
+        slideIndex = 1
     }
+    /*for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }*/
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    //dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 // for manual slide show, enable below code and remove onload=showslides() in slideshow.html page
-/*
 
+/*
 var slideIndex = 1;
 showSlides(slideIndex);
+
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -45,4 +48,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}*/
+}
+*/
+
